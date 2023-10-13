3b0fa1ff8da22d6c42914e2e6efa293ba5782e1f
HtayMinKhaung <htay.kaung@spiceworks.co.jp>
2023-10-13 14:56:10 +0630

    validateEmail

--- string.js
+++ string.js
@@ -1,3 +1,9 @@
 String.prototype.addHi = function () {
     return 'Hi !, ' + this.valueOf();
 }
+
+String.prototype.validateEmail = function () {
+    return this.valueOf().match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+}
