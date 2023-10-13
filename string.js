b7280a9833b52b6371cfb4f4d661691490936284
HtayMinKhaung <htay.kaung@spiceworks.co.jp>
2023-10-13 15:00:25 +0630

    string

--- string.js
+++ string.js
@@ -5,5 +5,5 @@ String.prototype.addHi = function () {
 String.prototype.validateEmail = function () {
     return this.valueOf().match(
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
+      ) !== null;
 }
